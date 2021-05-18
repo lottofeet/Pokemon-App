486e8902d7b8f3b77b7013cbf0b8c56ba752efaf
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-18 17:15:10 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-	{name: 'caterpie', height: 0.3, types: ['bug', 'electric']}
-	{name: 'sandslash', height: 1, type: ['ground']}
+	{name: 'caterpie', height: 0.3, types: ['bug', 'electric']},
+	{name: 'sandslash', height: 1, type: ['ground']},
 	{name: 'meowth', height: 0.4, type:['normal']}
 	]
 	for (let i = 0; i < pokemonList.length; i++);{
