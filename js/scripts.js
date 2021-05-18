8eb9912585af0533a585f616c0db0a900d2e97bf
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-18 18:01:40 -0500

    updated document write to show 'height'

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@ let pokemonList = [
 	{name: 'sandslash', height: 1, type: ['ground']},
 	{name: 'meowth', height: 0.4, type:['normal']},
 	]
-	for (let i = 0; i < pokemonList.length; i++);{
-		document.write(`${pokemonList.name}`);
+	for (let i = 0; i < pokemonList.length; i++){
+		document.write('pokemonList.name' + 'pokemonList.height');
 	}
