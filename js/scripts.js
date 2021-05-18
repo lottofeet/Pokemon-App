75d7cd96bb7d698ad66addd60cc9aaa9fb70b818
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-18 16:48:06 -0500

    Revert "Update scripts.js"
    This reverts commit c4b0564e3811b5159ee7cd48953929eaf43f230b.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,3 @@ let pokemonList = [
 	{name: 'sandslash', height: 1, type: ['ground']}
 	{name: 'meowth', height: 0.4, type:['normal']}
 	]
-	for (let i = 0; i < pokemonList.length);{
-		document.write(`${pokemonList.name} + ${pokemonList.height}`);
-	}
