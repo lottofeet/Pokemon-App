acc060d20a3375dda460e18aadf862393385620c
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-18 17:45:50 -0500

    update commas

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
 	{name: 'caterpie', height: 0.3, types: ['bug', 'electric']},
 	{name: 'sandslash', height: 1, type: ['ground']},
-	{name: 'meowth', height: 0.4, type:['normal']}
+	{name: 'meowth', height: 0.4, type:['normal']},
 	]
 	for (let i = 0; i < pokemonList.length; i++);{
-		document.write('pokemonList.name');
+		document.write(`${pokemonList.name}`);
 	}
