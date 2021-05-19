e8524217a2885b21454044a2434d890fe5e65036
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-19 17:58:06 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,16 @@ for (let i = 0; i < pokemonList.length; i++) {
 	}
 
 }	
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height}`);
+
+//   if (pokemonList[i].height > 0.9) {
+//     document.write("wow thats big");
+//   } else if (pokemonList[i].height < 0.5) {
+//     document.write("wow thats small");
+//   } else {
+//     document.write("wow, what an unusual size");
+//   }
+//   document.write("<br/>");
+// }`
