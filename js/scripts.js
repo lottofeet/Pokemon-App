c4de6cd70b9ed966c4b350c97987586e2fc0d3df
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-19 14:51:29 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-let pokemonList = [
-	{name: 'caterpie ', height: 0.3, types: ['bug', 'electric']},
-	{name: 'sandslash ', height: 1, type: ['ground']},
-	{name: 'meowth ', height: 0.4, type:['normal']},
-	]
+let pokemonList = [{
+	name: 'caterpie ', 
+	height: 0.3, 
+	types: ['bug', 'electric']
+  },
+  {
+	name: 'sandslash ',
+	height: 1,
+	type: ['ground']
+  },
+  {
+	name: 'meowth ',
+	height: 0.4,
+	type:['normal']
+  },
+]
 
 for (let i = 0; i < pokemonList.length; i++) {
-		document.write(pokemonList[i].name + 'height: ' + pokemonList[i].height);	
+	document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height}<br>`);	
+	}
+
+	if (`${pokemonList.height[i]}` >= 0.5) {
+		result('wow thats big')
+	} else if (pokemonList.height < 0.5) {
+		result('wow thats small')
+	} else {
+		result('wow, what an unusual size');
 	}
