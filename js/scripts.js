dd23510e4e099f05ddd6f086c1c4f15547d6f13d
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-19 11:36:21 -0500

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,5 @@ let pokemonList = [
 	{name: 'meowth', height: 0.4, type:['normal']},
 	]
 for (let i = 0; i < pokemonList.length; i++) {
-		document.write('pokemonList.name' + 'pokemonList.height');
+		document.write(pokemonList[i].name + pokemonList[i].height);
 	}
