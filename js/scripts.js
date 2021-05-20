7d71a1a2ab302eb1cc92547c9340c059da30955b
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-05-20 11:37:34 -0500

    Update js for-loop

--- js/scripts.js
+++ js/scripts.js
@@ -16,34 +16,11 @@ let pokemonList = [{
 ]
 
 
+
 for (let i = 0; i < pokemonList.length; i++) {
-if (pokemonList[i].height > 0.9) {
-document.write(
-${pokemonList[i].name} height: ${pokemonList[i].height} - Wow thats big <br/>);
-} else {
-document.write(${pokemonList[i].name} height: ${pokemonList[i].height} <br/>);
-}
-}
-
-
-// for (let i = 0; i < pokemonList.length; i++) {
-// 	if (pokemonList[i].height > 0.9 ) {
-// 		document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height} - Wow! That's big! <br>`);
-// 	} else {
-// 		document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height}  <br>`);
-// 	}
-
-// }	
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//   document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height}`);
-
-//   if (pokemonList[i].height > 0.9) {
-//     document.write("wow thats big");
-//   } else if (pokemonList[i].height < 0.5) {
-//     document.write("wow thats small");
-//   } else {
-//     document.write("wow, what an unusual size");
-//   }
-//   document.write("<br/>");
-// }`
+	if (pokemonList[i].height > 0.9 ) {
+		document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height} - Wow! That's big! <br>`);
+	} else {
+		document.write(`${pokemonList[i].name}  height:  ${pokemonList[i].height}  <br>`);
+	}
+}	
